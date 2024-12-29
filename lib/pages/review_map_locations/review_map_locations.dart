bdf3ec71f9dcf40e3d42c2d44b32f32e0b9fb4cb
yashodhaniroshan1999@gmail.com <newemail@example.com>
2024-12-29 12:19:25 +0530

    Added pages stubs for the reveiw list, review grid, photos and review map locations

---
+++ lib/pages/review_map_locations/review_map_locations.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class  extends StatefulWidget {
+  const ({super.key});
+static const  String route = '/review_map_locations';
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

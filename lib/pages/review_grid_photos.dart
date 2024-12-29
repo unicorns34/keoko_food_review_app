6098ff119f4a702b434f05a82dd42dde67d5eadd
yashodhaniroshan1999@gmail.com <newemail@example.com>
2024-12-29 11:32:25 +0530

    Added enums to create the app's constant values.

---
+++ lib/pages/review_grid_photos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReviewGridPhotos extends StatefulWidget {
+  const ReviewGridPhotos({super.key});
+  static const  String route = '/review_grid_photos';
+  @override
+  State<ReviewGridPhotos> createState() => _ReviewGridPhotosState();
+}
+
+class _ReviewGridPhotosState extends State<ReviewGridPhotos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/review_list/review_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReviewList extends StatefulWidget {
+  const ReviewList({super.key});
+
+  static const  String route = '/review_list';
+
+  @override
+  State<ReviewList> createState() => _ReviewListState();
+}
+
+class _ReviewListState extends State<ReviewList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

3e582725e2b77e03b44db6b767b1334789b84d3d
yashodhaniroshan1999@gmail.com <newemail@example.com>
2025-01-07 11:41:44 +0530

    Added review entry view, edit, photoZoom and authentication pages

---
+++ lib/pages/authentication/user_register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserRegister extends StatefulWidget {
+  const UserRegister({super.key});
+
+  static const String route = '/user-register';
+
+  @override
+  State<UserRegister> createState() => _UserRegisterState();
+}
+
+class _UserRegisterState extends State<UserRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
